766ee287557408aeadc741506cf5d9c299006645
Marcus Thorne <marcus_thorne475@hotmail.com>
2020-09-17 22:18:58 +0100

    database edits

--- db/migrate/20200917132150_create_purchases.rb
+++ db/migrate/20200917132150_create_purchases.rb
@@ -2,7 +2,6 @@ class CreatePurchases < ActiveRecord::Migration[6.0]
   def change
     create_table :purchases do |t|
       t.references :job
-      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
