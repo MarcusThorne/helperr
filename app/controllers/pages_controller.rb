1b50218eb9fb02f4de14cc5ab4c2ae40252d2673
Marcus Thorne <marcus_thorne475@hotmail.com>
2020-09-18 17:59:05 +0100

    bug fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,17 @@
 class PagesController < ApplicationController
-    skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: :home
 
-    def home
-        @job = Job.all.sample
-    end
+  def home
+    @job = Job.all.sample
+  end
 
-    def profile
-        @jobs = current_user.purchases.map do |purchase|
-                purchase.job
-            end
-        @jobs_sell = current_user.jobs
-    end
+  def profile
+    @jobs = current_user.purchases.map do |purchase|
+        purchase.job
+      end
+    @jobs_sell = current_user.jobs
+  end
 
-    def messages
-    end
+  def messages
+  end
 end
--- db/migrate/20200918121408_remove_columns_from_jobs.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnsFromJobs < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :jobs, :users_id
-  end
-end
