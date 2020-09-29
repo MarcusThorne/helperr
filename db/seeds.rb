30290fa59b4ea248133a1efe4fb5aa0fbf560e6c
marcusthorne <marcus_thorne475@hotmail.com>
2020-09-29 18:55:23 +0100

    added a image and new profession barber

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
-professions = [ "Plumbing", "Moving", "Shopping", "Electrical", "Carpentry", "Cleaning", "Painting", "General Helper", "Cooking", "Landscaping", "Gardening and Removal", "Computer Technician", "Car Mechanic", "Sewing", "Furniture Making", "Groundworks" ]
+professions = [ "Plumbing", "Moving", "Shopping", "Electrical", "Carpentry", "Cleaning", "Painting", "General Helper", "Cooking", "Landscaping", "Gardening and Removal", "Computer Technician", "Car Mechanic", "Sewing", "Furniture Making", "Groundworks", "Barber" ]
 
 300.times do
   user = User.create!(email: Faker::Internet.email, password: "123546", name: Faker::Name.name, rating: Faker::Number.within(range: 0.0..5.0), username: Faker::Internet.username, location: Faker::Address.city)
