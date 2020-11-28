b1e02ee08310f2d21e906dae4b609c4317bacfa3
marcusthorne <marcus_thorne475@hotmail.com>
2020-11-28 13:57:59 +0000

    styled featured a bit

--- app/javascript/components/navbarColor.js
+++ app/javascript/components/navbarColor.js
@@ -1,5 +1,10 @@
 const navbarColor = () => {
+  window.addEventListener('scroll', function() {
+    let navbar = document.querySelector('.navbar-content')
+    let windowPosition = window.scrollY > 80
 
+    navbar.classList.toggle('scrolling-active', windowPosition)
+  })
 }
 
 export {navbarColor};
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :purchases
   has_many :jobs
   has_one_attached :photo
+  has_one_attached :cover_photo
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
